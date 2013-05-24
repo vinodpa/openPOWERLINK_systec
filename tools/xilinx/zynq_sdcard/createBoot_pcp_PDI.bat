6c9b35d02804a9db65877a0fe8d2fd4b5bb2a891
Vinod PA <vinodpa@kalycito.com>
2013-05-24 11:46:55 +0530

    [TASK] Boot scripts Adapted for MN applications in Zynq

--- tools/xilinx/zynq_sdcard/createBoot_pcp_PDI.bat
+++
@@ -1,6 +0,0 @@
-cp ../../../fpga/xilinx/Xilinx_Z702/Zynq_ap_pcp_intaxi-axi/implementation/system.bit .
-cp ../../../zynq_fsbl_dualProcessor/Debug/zynq_fsbl_dualProcessor.elf .
-cp ../../../apps/ap_PDI/ap_pdi.elf .
-cp ../../../powerlink/pcp_PDI/pcp_PDI.elf .
-arm-xilinx-eabi-objcopy -I elf32-little -O binary pcp_PDI.elf pcp_PDI.bin
-bootgen -image bootimage_pcp_PDI.bif -o i BOOT.BIN -w on 
