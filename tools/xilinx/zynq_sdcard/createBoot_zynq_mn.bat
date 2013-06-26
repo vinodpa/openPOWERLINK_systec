a3dc3a81dba5a74931ea159756b238ea4ad43602
Vinod PA <vinodpa@kalycito.com>
2013-06-26 16:24:36 +0530

    [TASK] system updated for higher performance --> Makefile & setting are modified for update from root directory

--- tools/xilinx/zynq_sdcard/createBoot_zynq_mn.bat
+++ tools/xilinx/zynq_sdcard/createBoot_zynq_mn.bat
@@ -3,4 +3,7 @@ cp ../../../zynq_fsbl_dualProcessor/Debug/zynq_fsbl_dualProcessor.elf .
 cp ../../../examples/arch/zynq_cortexa9_0/no_os/gnu/demo_mn/demo_mn.elf .
 cp ../../../stack/make/driver/xilinx/zynq/mn_pcp/mn_pcp.elf .
 arm-xilinx-eabi-objcopy -I elf32-little -O binary mn_pcp.elf mn_pcp.bin
-bootgen -image bootimage_zynq_mn.bif -o i BOOT.BIN -w on 
+bootgen -image bootimage_zynq_mn.bif -o i BOOT.BIN -w on
+rm *.elf
+rm system.bit
+rm mn_pcp.bin
