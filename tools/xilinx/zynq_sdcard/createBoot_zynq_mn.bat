76d7450e94c63c5c3106999b9ad57409eb834656
Vinod PA <vinodpa@kalycito.com>
2013-06-27 11:01:17 +0530

    [TASK] Missed software files & system.bit updated --> bat modified for MNDK root as directory

--- tools/xilinx/zynq_sdcard/createBoot_zynq_mn.bat
+++ tools/xilinx/zynq_sdcard/createBoot_zynq_mn.bat
@@ -1,5 +1,5 @@
 cp ../../../fpga/boards/xilinx/xilinx_z702/Zynq_ap_pcp_intaxi-axi/implementation/system.bit .
-cp ../../../Workspace/zynq_fsbl_dualProcessor/Debug/zynq_fsbl_dualProcessor.elf .
+cp ../../../zynq_fsbl_dualProcessor/Debug/zynq_fsbl_dualProcessor.elf .
 cp ../../../examples/arch/zynq_cortexa9_0/no_os/gnu/demo_mn/demo_mn.elf .
 cp ../../../stack/make/driver/xilinx/zynq/mn_pcp/mn_pcp.elf .
 arm-xilinx-eabi-objcopy -I elf32-little -O binary mn_pcp.elf mn_pcp.bin
